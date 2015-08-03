637af45a67bac807052657dd91cb444e0e0dc093
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-03 10:32:31 -0600

    added exceptions

--- lib/brew_notify.rb
+++ lib/brew_notify.rb
@@ -1,5 +1,6 @@
 require "brew_notify/version"
 require "brew_notify/checker"
+require "brew_notify/exceptions"
 
 module BrewNotify
 end
--- lib/brew_notify/exceptions.rb
+++ lib/brew_notify/exceptions.rb
@@ -1,3 +1,4 @@
 require "exceptions/terminal_notify_error"
 require "exceptions/growl_error"
 require "execptions/gntp_error"
+require "exceptions/no_notifier_found_error"
