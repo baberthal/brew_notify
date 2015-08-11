1e431856deb0423dc0dca7da2ad2192a35f78404
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-11 12:48:08 -0600

    add templates and installation

--- lib/brew_notify.rb
+++ lib/brew_notify.rb
@@ -1,5 +1,12 @@
 require "brew_notify/version"
 require "brew_notify/checker"
+require "thor"
 
 module BrewNotify
+  class MyCLI < Thor
+    desc "hello NAME", "say hello to NAME"
+    def hello(name)
+      puts "Hello #{name}"
+    end
+  end
 end
